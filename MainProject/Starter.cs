24a85bf4c4dbac87c89d8f04bf3c5c64e6b70721
Martin Perzl <Parcley@users.noreply.github.com>
2021-07-06 07:31:41 +0200

    initial commit

---
+++ MainProject/Starter.cs
@@ -0,0 +1,19 @@
+// <copyright file="Starter.cs" company="Rohde &amp; Schwarz GmbH &amp; Co. KG">
+// Copyright (c) Rohde &amp; Schwarz GmbH &amp; Co. KG. All rights reserved.
+// </copyright>
+
+namespace MainProject
+{
+    /// <summary>
+    /// starter class.
+    /// </summary>
+    public static class Starter
+    {
+        /// <summary>
+        /// an empty main function.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
